dad8797f68236b06c1fc62c15f54f0a2b1ba8f19
Abhishek Vaze <abhishek.vaze@resurs.se>
2025-01-11 21:14:51 +0100

    my cars

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,36 +2,61 @@ import React from "react";
 import { useLocation } from "react-router-dom";
 
 const Home = () => {
-  const location = useLocation();
-  const cars = location.state?.cars || [];
+    const location = useLocation();
+    const cars = location.state?.cars || [];
 
-  return (
-    <div className="container mt-5">
-      <h1>Your Cars</h1>
-      <div className="row">
-        {cars.map((car) => (
-          <div key={car.carId} className="col-md-4">
-            <div className="card mb-4">
-              <img
-                src={car.imageLinks?.[0] || "https://via.placeholder.com/150"}
-                className="card-img-top"
-                alt={`${car.make} ${car.model}`}
-              />
-              <div className="card-body">
-                <h5 className="card-title">{car.make} {car.model}</h5>
-                <p className="card-text">
-                  <strong>Year:</strong> {car.year} <br />
-                  <strong>City:</strong> {car.city} <br />
-                  <strong>Likes:</strong> {car.likeCount} <br />
-                  <strong>Dislikes:</strong> {car.dislikeCount}
-                </p>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div className="container mt-5">
+            <h1>Your Cars</h1>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Model</th>
+                        <th>Year</th>
+                        <th>City</th>
+                        <th>Likes</th>
+                        <th>Dislikes</th>
+                        <th>Deals</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {cars.map((car, index) => (
+                        <tr key={car.carId}>
+                            <td>{index + 1}</td>
+                            <td>{car.make} {car.model}</td>
+                            <td>{car.year}</td>
+                            <td>{car.city}</td>
+                            <td>{car.likeCount}</td>
+                            <td>{car.dislikeCount}</td>
+                            <td>{car.dealCount}</td>
+                            <td>
+                                <button
+                                    className="btn btn-danger btn-sm me-2"
+                                    onClick={() => window.open(car.imageLinks?.[0] || "https://via.placeholder.com/150", "_blank")}
+                                >Remove
+                                </button>
+                                <button
+                                    className="btn btn-success btn-sm me-2"
+                                    onClick={() => window.open(car.imageLinks?.[0] || "https://via.placeholder.com/150", "_blank")}
+                                >
+                                    Deals
+                                </button>
+                                <button
+                                    className="btn btn-primary btn-sm me-2"
+                                    onClick={() => window.open(car.imageLinks?.[0] || "https://via.placeholder.com/150", "_blank")}
+                                >
+                                    Image
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+
+    );
 };
 
 export default Home;
